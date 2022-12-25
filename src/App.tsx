1ae0e725484f204fcd6002d28ec7268146c78077
jstephensdev <jessica.stephens.dev@gmail.com>
2022-12-25 09:33:16 -0600

    info and settings icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Keyboard } from './components/Keyboard';
+import IonIcon from '@reacticons/ionicons';
 import './css/App.css';
 
 export const App = () => {
     return (
-        <div className="app">
-            <header>
+        <>
+            <header className="app-header">
+                <IonIcon name="help-circle-outline" size="large" />
                 <h1>Ad Free Typing</h1>
+                <IonIcon name="settings-outline" size="large" />
             </header>
-            <Keyboard />
-        </div>
+            <div className="app">
+                <Keyboard />
+            </div>
+        </>
     );
 };
