da5c0ea0ecff7b73759f6f8e6d3057960b79f5ea
jstephensdev <jessica.stephens.dev@gmail.com>
2022-12-25 17:02:28 -0600

    faker service, tsconfig checking in dirs in src

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,6 +1,6 @@
 import { keyboardConfig, key } from '../keyboardConfig';
 import { useEffect, useState } from 'react';
-import { faker } from '@faker-js/faker';
+import { text } from '../services/text';
 
 export const Keyboard = () => {
     const keyRows: Array<Array<key>> = keyboardConfig;
@@ -11,7 +11,7 @@ export const Keyboard = () => {
 
     const currentTime = () => new Date().getTime();
 
-    const initialText = faker.lorem.paragraph();
+    const initialText = text();
 
     const [leftPadding, setLeftPadding] = useState(
         new Array(30).fill(' ').join('')
