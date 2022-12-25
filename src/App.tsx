a1715d9f711d2516adf30e2c9b4f6151a3c8b18b
jstephensdev <jessica.stephens.dev@gmail.com>
2022-12-25 09:41:11 -0600

    github link

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ export const App = () => {
     return (
         <>
             <header className="app-header">
-                <IonIcon name="help-circle-outline" size="large" />
+                <div>
+                    <IonIcon name="help-circle-outline" size="large" />
+                    <a
+                        href="https://github.com/jstephensdev/ad-free-typing"
+                        target="_blank"
+                    >
+                        <IonIcon name="logo-github" size="large" />
+                    </a>
+                </div>
                 <h1>Ad Free Typing</h1>
                 <IonIcon name="settings-outline" size="large" />
             </header>
