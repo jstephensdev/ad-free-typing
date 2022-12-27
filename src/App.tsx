8827e7dda34430e4b038e773020da197397c62ad
jstephensdev <jessica.stephens.dev@gmail.com>
2022-12-26 18:13:47 -0600

    extract what is not keyboard related, app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Keyboard } from './components/Keyboard';
-import { Header } from './components/Header';
 import './css/App.css';
+import { Main } from './components/Main';
+import { Header } from './components/Header';
+import { Keyboard } from './components/Keyboard';
 
 export const App = () => {
     return (
         <>
             <Header />
             <div className="app">
+                <Main />
                 <Keyboard />
             </div>
         </>
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,108 +1,12 @@
 import { KeyboardRows, key } from '../KeyboardRows';
-import { useKeyDetection } from '../hooks/useKeyDetection';
-import { useState } from 'react';
-import { fakerText } from '../services/faker';
-import { currentTime } from '../services/currentTime';
 import { useAppSelector } from '../hooks/reduxHooks';
 
 export const Keyboard = () => {
     const keyRows: Array<Array<key>> = KeyboardRows;
-
-    const [startTime, setStartTime] = useState(0);
-    const [wordCount, setWordCount] = useState(0);
-    const [wpm, setWpm] = useState('00.00');
-
-    const initialText = fakerText();
-
-    const [leftPadding, setLeftPadding] = useState(
-        new Array(30).fill(' ').join('')
-    );
-    const [outgoingChars, setOutgoingChars] = useState('');
-    const [currentChar, setCurrentChar] = useState(initialText.charAt(0));
-    const [incomingChars, setIncomingChars] = useState(
-        initialText.substring(1)
-    );
-
-    const [accuracy, setAccuracy] = useState('000.00');
-    const [typedChars, setTypedChars] = useState('');
-
     const keyPressed = useAppSelector((state) => state.keyPressed.value);
-
-    useKeyDetection((key) => {
-        let updatedOutgoingChars = outgoingChars;
-        let updatedIncomingChars = incomingChars;
-        const updatedTypedChars = typedChars + key;
-
-        if (key === currentChar) {
-            if (startTime === 0) {
-                setStartTime(currentTime());
-            }
-            if (leftPadding.length > 0) {
-                setLeftPadding(leftPadding.substring(1));
-            }
-
-            setOutgoingChars((updatedOutgoingChars += currentChar));
-            setTypedChars(updatedTypedChars);
-            setCurrentChar(incomingChars.charAt(0));
-
-            updatedIncomingChars = incomingChars.substring(1);
-            if (updatedIncomingChars.split(' ').length < 10) {
-                updatedIncomingChars += ' ' + initialText;
-            }
-            setIncomingChars(updatedIncomingChars);
-            if (incomingChars.charAt(0) === ' ') {
-                setWordCount(wordCount + 1);
-                const durationInMinutes = (currentTime() - startTime) / 60000.0;
-                setWpm(((wordCount + 1) / durationInMinutes).toFixed(2));
-            }
-        }
-
-        if (
-            updatedOutgoingChars.length <= updatedTypedChars.length &&
-            updatedOutgoingChars.length > 0 &&
-            key.length === 1
-        ) {
-            setTypedChars(updatedTypedChars);
-            setAccuracy(
-                (
-                    (updatedOutgoingChars.length * 100) /
-                    updatedTypedChars.length
-                ).toFixed(2)
-            );
-        }
-    });
 
     return (
         <>
-            <section className="stats">
-                <span>{`Duration: `}</span>
-                <span className="duration">{`${
-                    startTime === 0
-                        ? '00:00'
-                        : new Date(currentTime() - startTime)
-                              .toLocaleTimeString('en-US')
-                              .slice(2, 7)
-                }`}</span>
-                <span>{` | WPM: `}</span>
-                <span className="wpm">{`${wpm}`}</span>
-                <span>{` | ACC: `}</span>
-                <span className="acc">{`${accuracy}%`}</span>
-                <span>{` | Error Rate: `}</span>
-                <span className="error-rate">{`${
-                    accuracy === '000.00'
-                        ? '000.00'
-                        : (100 - Number(accuracy)).toFixed(2)
-                }%`}</span>
-            </section>
-            <section className="text-section">
-                <p>
-                    <span className="character-out">
-                        {(leftPadding + outgoingChars).slice(-30)}
-                    </span>
-                    <span className="character-current">{currentChar}</span>
-                    <span>{incomingChars.substring(0, 30)}</span>
-                </p>
-            </section>
             <div className="keyboard">
                 {keyRows.map((row: Array<key>, index) => (
                     <div className="keyboard-row" key={index}>
