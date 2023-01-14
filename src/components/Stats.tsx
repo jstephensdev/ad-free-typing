b1beb098c2a24b29349d59ed66dacbadfc7a6f1b
jstephensdev <jessica.stephens.dev@gmail.com>
2023-01-13 20:30:15 -0600

    layout improvements

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -15,25 +15,29 @@ export const Stats = () => {
                     variant="success"
                     now={(outgoingChars.length / text.length) * 100}
                 />
-                <span>
-                    {`Duration: `} &nbsp; <Timer />
-                </span>
+                <div style={{ marginTop: '10px'}}>
+                    <span>
+                        {`Duration: `} &nbsp; <Timer />
+                    </span>
 
-                <span>
-                    {` | WPM:`} &nbsp; <span className="wpm">{`${wpm}`}</span>
-                </span>
+                    <span>
+                        {` | WPM:`} &nbsp;
+                        <span className="wpm">{`${wpm}`}</span>
+                    </span>
 
-                <span>
-                    {` | ACC: `} &nbsp; <span className="acc">{`${acc}%`}</span>
-                </span>
-                <span>
-                    {` | Error Rate: `} &nbsp;
-                    <span className="error-rate">{`${
-                        acc === '000.00'
-                            ? '000.00'
-                            : (100 - Number(acc)).toFixed(2)
-                    }%`}</span>
-                </span>
+                    <span>
+                        {` | ACC: `} &nbsp;
+                        <span className="acc">{`${acc}%`}</span>
+                    </span>
+                    <span>
+                        {` | Error Rate: `} &nbsp;
+                        <span className="error-rate">{`${
+                            acc === '000.00'
+                                ? '000.00'
+                                : (100 - Number(acc)).toFixed(2)
+                        }%`}</span>
+                    </span>
+                </div>
             </section>
         </>
     );
