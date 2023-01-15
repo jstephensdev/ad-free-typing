c89c41fcfb63fe0e8c6eb9c012f24bad792572f2
jstephensdev <jessica.stephens.dev@gmail.com>
2023-01-15 10:13:38 -0600

    redux state out of sync, might be causing the routing issue

--- src/store/store.ts
+++ src/store/store.ts
@@ -36,6 +36,11 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred types: stats, text
 export type AppDispatch = typeof store.dispatch;
 
+// Update redux window location on initial page load and if page reload
+store.dispatch(
+    setWindowLocation(JSON.parse(JSON.stringify(window.location)))
+);
+
 // Update Redux if we navigated via browser's back/forward
 window.addEventListener('popstate', () => {
     store.dispatch(
