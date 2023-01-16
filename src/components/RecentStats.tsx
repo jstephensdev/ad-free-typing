e031737fd65ceb24d223d692ed3276a150fb284f
jstephensdev <jessica.stephens.dev@gmail.com>
2023-01-16 09:37:07 -0600

    bootstrap container to center and horizontally pad

--- src/components/RecentStats.tsx
+++ src/components/RecentStats.tsx
@@ -10,6 +10,7 @@ import { setText, TextMode } from '../store/slices/textSlice';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 interface Props {
     recentStats: RecentStat[];
@@ -28,81 +29,85 @@ export const RecentStats = ({ recentStats }: Props) => {
     return (
         <>
             <section>
-                <div className="ionIcon" onClick={() => updateText()}>
-                    <IonIcon
-                        title="New Round"
-                        name="arrow-back-circle-outline"
-                        size="large"
-                        data-testid="new-text-reset"
-                    />
-                    <p>New Round</p>
-                </div>
-                <div>
-                    <h6>Rounds (12 Most Recent)</h6>
-                </div>
-                <Row>
-                    {recentStats?.length ? (
-                        recentStats.map((stat, index) => (
-                            <Card
-                                key={index}
-                                style={{ width: '18rem', margin: '1rem' }}
-                            >
-                                <Card.Header>
-                                    <p>{stat?.timeDateStamp}</p>
-                                </Card.Header>
-                                <ListGroup variant="flush">
-                                    <ListGroup.Item>
-                                        <span className="mode">
-                                            {stat?.mode}
-                                        </span>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <span>Duration:</span>
-                                        <span className="duration">
-                                            {stat?.duration}
-                                        </span>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <span>WPM:</span>
-                                        <span className="wpm">{stat?.wpm}</span>
-                                    </ListGroup.Item>
-
-                                    <ListGroup.Item>
-                                        <span>ACC:</span>
-                                        <span className="acc">
-                                            {stat?.acc}%
-                                        </span>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <span>Error Rate:</span>
-                                        <span className="error-rate">
-                                            {stat?.errorRate}%
-                                        </span>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Card>
-                        ))
-                    ) : (
-                        <>
-                            <p>Complete a Round</p>
-                        </>
-                    )}
-                </Row>
-                {recentStats?.length ? (
-                    <div
-                        className="clear"
-                        onClick={() => dispatch(removeAllStats())}
-                    >
+                <Container>
+                    <div className="ionIcon" onClick={() => updateText()}>
                         <IonIcon
-                            name="close-circle-outline"
+                            title="New Round"
+                            name="arrow-back-circle-outline"
                             size="large"
-                            data-testid="reset"
+                            data-testid="new-text-reset"
                         />
-                        <p>Clear All Rounds</p>
+                        <p>New Round</p>
                     </div>
-                ) : (
-                    <></>
-                )}
+                    <div>
+                        <h6>Rounds (12 Most Recent)</h6>
+                    </div>
+                    <Row>
+                        {recentStats?.length ? (
+                            recentStats.map((stat, index) => (
+                                <Card
+                                    key={index}
+                                    style={{ width: '18rem', margin: '1rem' }}
+                                >
+                                    <Card.Header>
+                                        <p>{stat?.timeDateStamp}</p>
+                                    </Card.Header>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>
+                                            <span className="mode">
+                                                {stat?.mode}
+                                            </span>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item>
+                                            <span>Duration:</span>
+                                            <span className="duration">
+                                                {stat?.duration}
+                                            </span>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item>
+                                            <span>WPM:</span>
+                                            <span className="wpm">
+                                                {stat?.wpm}
+                                            </span>
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            <span>ACC:</span>
+                                            <span className="acc">
+                                                {stat?.acc}%
+                                            </span>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item>
+                                            <span>Error Rate:</span>
+                                            <span className="error-rate">
+                                                {stat?.errorRate}%
+                                            </span>
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Card>
+                            ))
+                        ) : (
+                            <>
+                                <p>Complete a Round</p>
+                            </>
+                        )}
+                    </Row>
+                    {recentStats?.length ? (
+                        <div
+                            className="clear"
+                            onClick={() => dispatch(removeAllStats())}
+                        >
+                            <IonIcon
+                                name="close-circle-outline"
+                                size="large"
+                                data-testid="reset"
+                            />
+                            <p>Clear All Rounds</p>
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                </Container>
             </section>
         </>
     );
