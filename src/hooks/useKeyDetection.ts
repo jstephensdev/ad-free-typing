1f9f3d02cfea3a4264c3f34498ebccde99092c29
“Jessica <jessica.stephens.dev@gmail.com>
2023-11-07 23:21:05 -0600

    formatting, fixing logic for capslock

--- src/hooks/useKeyDetection.ts
+++ src/hooks/useKeyDetection.ts
@@ -1,57 +1,52 @@
 import { useEffect, useState } from 'react';
 
 const keysToRegister: string[] = [
-    'CapsLock',
-    'Shift',
-    'Control',
-    'WakeUp',
-    'Alt',
-    'Tab',
-    'Backspace',
+  'CapsLock',
+  'Shift',
+  'Control',
+  'WakeUp',
+  'Alt',
+  'Tab',
+  'Backspace',
 ];
 
 export const useKeyDetection = (callback: (key: string) => void): string => {
-    const [keyPress, setKeyPress] = useState('');
+  const [keyPress, setKeyPress] = useState('');
 
-    useEffect(() => {
-        const downHandler = (e: KeyboardEvent): void => {
-            // if keyPress is CapsLock and key is not CapsLock, CapsLock is on
-            if (keyPress === keysToRegister[0] && e.key !== keysToRegister[0]) {
-                setKeyPress(keysToRegister[0]);
-                return;
-            }
-            // if keyPress is CapsLock and key is CapsLock, CapsLock is not on
-            if (keyPress === keysToRegister[0] && keysToRegister[0] === e.key) {
-                setKeyPress('');
-                return;
-            }
-            if (
-                keyPress !== e.key ||
-                e.key.length === 1 ||
-                keysToRegister.includes(e.key)
-            ) {
-                setKeyPress(e.key);
-                callback && callback(e.key);
-            }
-        };
+  useEffect(() => {
+    const downHandler = (e: KeyboardEvent): void => {
+      // prevents other key press until capslock is off
+      if (keysToRegister[0] === e.key || keysToRegister[0] === keyPress) {
+        setKeyPress(keysToRegister[0]);
+        return;
+      }
+      if (
+        keyPress !== e.key ||
+        e.key.length === 1 ||
+        keysToRegister.includes(e.key)
+      ) {
+        setKeyPress(e.key);
+        callback && callback(e.key);
+      }
+    };
 
-        const upHandler = (e: KeyboardEvent): void => {
-            // if keyPress is CapsLock and the key is not CapsLock, CapsLock is on
-            if (keyPress === keysToRegister[0] && keysToRegister[0] !== e.key) {
-                setKeyPress(keysToRegister[0]);
-                return;
-            }
-            if (keysToRegister[0] !== e.key) {
-                setKeyPress('');
-            }
-        };
-        window.addEventListener('keydown', downHandler);
-        window.addEventListener('keyup', upHandler);
+    const upHandler = (e: KeyboardEvent): void => {
+      // prevents capslock from appearing off if shift key is selected
+      if (keysToRegister[0] === keyPress && keysToRegister[1] === e.key) {
+        return;
+      }
+      // deselect keysToRegister when condition above is not true
+      if (keysToRegister.includes(e.key)) {
+        setKeyPress('');
+      }
+    };
+    window.addEventListener('keydown', downHandler);
+    window.addEventListener('keyup', upHandler);
 
-        return () => {
-            window.removeEventListener('keydown', downHandler);
-            window.removeEventListener('keyup', upHandler);
-        };
-    });
-    return keyPress;
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+      window.removeEventListener('keyup', upHandler);
+    };
+  });
+  return keyPress;
 };
