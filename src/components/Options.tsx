8d0b03269ea2e3e0bb4632868d565e88828a7fba
“Jessica <jessica.stephens.dev@gmail.com>
2023-11-21 19:47:35 -0600

    prettier formatting and adjusting options css

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -4,39 +4,37 @@ import { setText, TextMode, modes } from '../store/slices/textSlice';
 import { setUrl } from '../store/slices/routingSlice';
 
 export const Options = (): JSX.Element => {
-    const dispatch = useAppDispatch();
-    const mode = useAppSelector((state) => state.text.mode);
+  const dispatch = useAppDispatch();
+  const mode = useAppSelector((state) => state.text.mode);
 
-    const radioOptionChange = (textMode: TextMode): void => {
-        dispatch(resetStats());
-        dispatch(setText(textMode));
-        dispatch(setUrl('/'));
-    };
+  const radioOptionChange = (textMode: TextMode): void => {
+    dispatch(resetStats());
+    dispatch(setText(textMode));
+    dispatch(setUrl('/'));
+  };
 
-    const renderRadioOption = (modes: TextMode[]): JSX.Element[] => {
-        return modes.map((modeOption: TextMode) => (
-            <label htmlFor={modeOption} key={modeOption}>
-                <input
-                    style={{ margin: "20px",}}
-                    key={modeOption}
-                    data-testid={modeOption}
-                    aria-label={modeOption}
-                    type="checkbox"
-                    checked={mode === modeOption}
-                    onChange={() => radioOptionChange(modeOption)}
-                />
-                {modeOption}
-            </label>
-        ));
-    };
+  const renderRadioOption = (modes: TextMode[]): JSX.Element[] => {
+    return modes.map((modeOption: TextMode) => (
+      <label htmlFor={modeOption} key={modeOption}>
+        <input
+          style={{ margin: '15px'}}
+          key={modeOption}
+          data-testid={modeOption}
+          aria-label={modeOption}
+          type="radio"
+          checked={mode === modeOption}
+          onChange={() => radioOptionChange(modeOption)}
+        />
+        {modeOption}
+      </label>
+    ));
+  };
 
-    return (
-        <>
-            <section>
-                <section className="options">
-                    {renderRadioOption(modes)}
-                </section>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section>
+        <section>{renderRadioOption(modes)}</section>
+      </section>
+    </>
+  );
 };
